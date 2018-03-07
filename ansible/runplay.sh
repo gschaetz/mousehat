1da4b33319603b1827f375c3360fdb7f01fef6f8
Gary Schaetz <gary@schaetzkc.com>
2018-03-06 22:06:22 -0600

    various fixes and updated readme

--- ansible/runplay.sh
+++ ansible/runplay.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+while getopts ":p:s:" opt; do
+  case $opt in
+    p)
+      echo "Using desktop directory: $OPTARG"
+      export DESKTOP_PROJECT_DIR=$OPTARG 
+      ;;
+    s)
+      echo "Using desktop setting directory: $OPTARG"
+      export DESKTOP_SETTINGS_DIR=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
 ansible-playbook -v -i "localhost," -c local provdesktop.yml 
